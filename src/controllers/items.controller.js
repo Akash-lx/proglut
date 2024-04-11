39cac02712bdb45efa64ef30122d934bb92d0a65
Akash-lx <akash.logixhunt@gmail.com>
2024-04-11 17:30:33 +0530

    bug in itemlist api

--- src/controllers/items.controller.js
+++ src/controllers/items.controller.js
@@ -1,4 +1,4 @@
-// import mongoose, {isValidObjectId} from "mongoose"
+import mongoose, {isValidObjectId} from "mongoose"
 import { Item } from "../models/items.model.js"
 // import {User} from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
