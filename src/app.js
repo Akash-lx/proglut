1970955c998333b9600244b916736bc5996a70d6
Akash-lx <akash.logixhunt@gmail.com>
2024-05-15 17:21:25 +0530

    payload size limit

--- src/app.js
+++ src/app.js
@@ -14,8 +14,9 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.json({ limit: "50MB" }))
+// app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: '50MB', parameterLimit: 100000, fieldSize: 10000000 }));
 app.use(express.static("public"))
 app.use(cookieParser())
 
